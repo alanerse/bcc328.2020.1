3b17c09b697b049b1ba6ada515469b6efb13e42a
Alan Vasconcellos Erse <alan.erse@sydle.com>
2021-04-21 18:47:32 -0300

    feat: add semantic analysis

--- src/driver/driver.ml
+++ src/driver/driver.ml
@@ -38,6 +38,10 @@ let main () =
       let boxtree = Tree.box_of_tree tree in
       print_endline (Box.string_of_box boxtree);
       print_endline (Tree.string_of_tree tree)
+      print_endline "Analise semântica:";
+      print_endline "============================================================";
+      Semantic.check_program(ast);
+
   with
   | Error.Error (loc, msg) ->
      Format.printf "%a error: %s\n" Location.pp_location loc msg;
